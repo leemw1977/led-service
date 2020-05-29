674f6c37c2950686ca00dc02a16a98e2be18ed03
Lee Williams <leemw1977@outlook.com>
2020-05-29 17:44:34 +0100

    initial projects

---
+++ led-service-api/Controllers/LedController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using led_service_domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace led_service_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LedController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get(int red, int green, int blue)
+        {
+            var ledService = new LedService();
+            ledService.SetColour(red, green, blue);
+            return Ok();
+        }
+    }
+}
---
+++ led-service-domain/LedService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace led_service_domain
+{
+    public class LedService
+    {
+        public void SetColour(int red, int green, int blue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
